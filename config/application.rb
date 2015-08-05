f0a397597e7ef792c8eaa8983a84f05dfaf92944
Vaillant <vaillant.jeremy@dev-crea.com>
2015-08-05 15:40:21 +0200

    Change default config

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require "rails"
-# Pick the frameworks you want:
 require "active_model/railtie"
-require "active_job/railtie"
-# require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
+require "active_support/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -17,16 +14,10 @@ Bundler.require(*Rails.groups)
 
 module Modele
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.locale = :fr
+    config.i18n.default_locale = :fr
+    config.assets.css_compressor = :yui
+    config.assets.js_compressor = :yui
+    config.mongoid.logger = Logger.new($stdout, :info)
   end
 end
