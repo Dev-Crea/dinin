8caafa9b7d4202f16c70ad0eac5d7516a80e37b0
Jeremy VAILLANT <jeremy@network.fr>
2016-04-25 16:19:51 +0200

    Fix address to smtp

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   user_name: ENV['SMTP_USER'],
   password: ENV['SMTP_PASSWORD'],
   domain: 'd2go.fr',
-  address: 'mail.gandi',
+  address: 'mail.gandi.net',
   port: 587,
   authentification: :plain,
   enable_starttls_auto: true
