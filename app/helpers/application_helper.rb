b91a77047cb8202673fa427ab70e713c546dde86
Jeremy VAILLANT <jeremy@network.fr>
2016-04-25 16:39:27 +0200

    Update submit button with foundation class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 # Generic helper
 module ApplicationHelper
+  # Submit button
+  def submit_button(resource, text)
+    resource.submit text, class: 'button'
+  end
+
   # Font awesome icon helper
   def fa_icon(names = 'flag', options = {})
     classes = ['fa']
