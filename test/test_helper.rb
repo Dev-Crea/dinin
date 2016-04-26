3a7aa3a04c5cea5b11be98951848b735fd1f10cd
Jeremy VAILLANT <jeremy@network.fr>
2016-04-26 11:22:55 +0200

    Fix rubocop rule

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-class ActiveSupport
-  class TestCase
-    # Add more helper methods to be used by all tests here...
-  end
+# rubocop:disable Style/ClassAndModuleChildren
+class ActiveSupport::TestCase
+  # Add more helper methods to be used by all tests here...
 end
+# rubocop:enable Style/ClassAndModuleChildren
