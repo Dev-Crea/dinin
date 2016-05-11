56a087319238e0814dd24e0faf58e281a8ce82a8
Jeremy VAILLANT <jeremy@network.fr>
2016-05-11 17:47:07 +0200

    Refractor view flux

--- app/helpers/flux_helper.rb
+++ app/helpers/flux_helper.rb
@@ -1,3 +1,15 @@
 # Flux helper
 module FluxHelper
+  def button_group_link(site)
+    link_to site.domaine, class: 'button primary' do
+      fa_icon 'link', text: site.domaine
+    end
+  end
+
+  def button_group_rss(site)
+    link_to news_path(site_id: site.id, format: 'atom'),
+            class: 'button success' do
+      fa_icon 'rss', text: t('flux.shared.souscrire', site: site.nom)
+    end
+  end
 end
