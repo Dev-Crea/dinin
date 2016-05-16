259b12e9b0ee7e3329877a53003569b637600b64
Vaillant Jeremy <vaillant.jeremy@dev-crea.com>
2016-05-16 20:48:15 +0200

    Fix closable message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
   def submit_button(resource, text)
     resource.submit text, class: 'button'
   end
+
+  # Closable button
+  def closable
+    classes = 'close-button'
+    button_tag :button, class: classes, 'aria-label': 'Dismiss alert',
+                        'data-close': true do
+      content_tag :span, fa_icon('times'), 'aria-hidden': true
+    end
+  end
 end
