d1f625694517c79968a82f6d66975ad03279d2da
Vaillant Jeremy <vaillant.jeremy@dev-crea.com>
2016-05-16 21:34:26 +0200

    Fix closing message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,11 +6,39 @@ module ApplicationHelper
   end
 
   # Closable button
-  def closable
-    classes = 'close-button'
-    button_tag :button, class: classes, 'aria-label': 'Dismiss alert',
-                        'data-close': true do
-      content_tag :span, fa_icon('times'), 'aria-hidden': true
+  def closable(notice, alert)
+    if notice || alert
+      content_tag :div, class: close_classes(notice, alert),
+                        'data-closable': '', id: 'message' do
+        capture do
+          concat closable_message(notice, alert)
+          concat closable_action
+        end
+      end
     end
+  end
+
+  private
+
+  def closable_message(notice, alert)
+    content_tag :p do
+      capture do
+        concat notice
+        concat alert
+      end
+    end
+  end
+
+  def closable_action
+    content_tag :button, class: 'close-button', 'aria-label': 'Dismiss alert',
+                         'data-close': '', type: 'button' do
+      content_tag :span, '&times;'.html_safe, 'aria-hidden': true
+    end
+  end
+
+  def close_classes(notice, alert)
+    classes = 'fixed-bottom callout text-center '
+    classes << 'success' if notice
+    classes << 'alert' if alert
   end
 end
