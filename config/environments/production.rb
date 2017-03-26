183d77d90c6b8dab5de3d9eea395e072bb1ace8e
Vaillant Jeremy <vaillant.jeremy@dev-crea.com>
2017-03-26 18:33:34 +0200

    Fix name varaible

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {
     host: Settings.host,
     protocol: Settings.protocol,
-    port: Setings.port
+    port: Settings.port
   }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
